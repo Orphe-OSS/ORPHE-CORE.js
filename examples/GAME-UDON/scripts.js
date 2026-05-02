605ef8d82e1ea957fc591f16e9d8d2ca0926ab7a
kikyu <kiku@orphe.io>
2026-05-02 19:25:24 +0900

    feat(examples): apply CoreToolkit BLE browser guard

--- examples/GAME-UDON/scripts.js
+++ examples/GAME-UDON/scripts.js
@@ -9,6 +9,10 @@ bles[0].setup();
 buildCoreToolkit(document.querySelector('#toolkit_placeholder1'), '01', 0, 'SENSOR_VALUES');
 bles[1].setup();
 buildCoreToolkit(document.querySelector('#toolkit_placeholder2'), '02', 1, 'SENSOR_VALUES');
+guardCoreToolkitBluetooth({
+    coreIds: [0, 1],
+    messageElement: '#ble-support-message'
+});
 for (let ble of bles) {
     ble.onConnect = function () {
         connectedDevices++;
